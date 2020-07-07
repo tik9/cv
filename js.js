9b029714d9aad93c1052f66e81ded0b9905cd008
Timo Körner <Lenovo@work>
2020-07-07 08:34:23 +0200

    js mit englisch

---
+++ js.js
@@ -0,0 +1,64 @@
+<script>
+
+    window.onload = function(){
+    var geburtstag = Date.parse("1978-10-02")
+    var alter = Date.now() - geburtstag
+    var msProJahr = 31536000000
+    var alter = Math.floor(alter / msProJahr);
+
+    werte = {
+        alter: {
+        en: alter,
+        },
+
+        name: {
+        en: 'Timo Körner'},
+
+        adresse:{
+        en: 'Bergstr. 15, 85120 Hepberg'},
+
+        job_titel: {
+        de: 'Technischer Oberinspektor',
+        'en' : 'IT expert',
+        },
+
+        job_sys: {
+        de:'Systemadministrator für Windows Server und Exchange',
+        en: Administrator Windows
+        },
+
+        job_freib:{
+        de: 'Freiberuflicher IT Spezialist',
+            en: Freelancer
+        },
+
+        an: {
+        de: 'Autobahndirektion Nordbayern',
+        en: 'Railway public services'
+        }
+
+        h_studium: {
+        de: 'Studium Diplom - Verwaltungsinformatik',
+        en: 'Bachelor IT'
+        }
+    }
+
+
+for (schluessel in werte) {
+		if (werte[schluessel]['de']){
+			sprache='de'
+        
+		}
+		else
+		{
+		sprache='en'
+		}
+		document.body.innerHTML = document.body.innerHTML.replace('{{ ' + schluessel + ' }}', werte[schluessel][sprache])
+//        console.log(schluessel + ' ' + werte[schluessel]['en']);
+        console.log(schluessel + ' ' + werte[schluessel][sprache]);
+
+        // console.log(schluessel);
+       
+}
+}
+</script>
