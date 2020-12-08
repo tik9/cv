d9d2ff4ad18914694655d97939ca6b9e47f34314
tik9 <len-win-user>
2020-12-08 21:36:10 +0100

    wanted-to-put-async-js-files-github-to-index-but-no

--- media/links.js
+++ media/links.js
@@ -1,23 +1,25 @@
 window.onload = function () {
 
-    // console.log('test links')
     ich = 'Timo Körner'
-    document.getElementById("tk").innerHTML = '<h1>' + ich + '</h1>'
-    // $('title').text()= ich
+    h1 = document.createElement('h1')
+    document.getElementById("tk").appendChild(h1)
+    h1.textContent = ich
     document.title = ich
 
     linkstop = {
         // LinkedIn: 'https://linkedin.com/in/timo-körner-65ab601b1',
-        Home:'index.html',
+        Home: 'index.html',
         Further: 'file.html',
+        Source: 'https://github.com/tik9/cv/blob/master/index.html'
     }
 
     about = [
-        'CEO of my life', 'CTO of localhosts', 'COO of my Flat'
+        'CEO of my life', 'CTO of localhosts', 
+        // 'COO of my Flat'
     ]
 
     toplinks = document.createElement('ul')
-    Object.keys(linkstop).forEach(function (key){
+    Object.keys(linkstop).forEach(function (key) {
         toplinksitem = document.createElement('li')
         toplinks.appendChild(toplinksitem)
         a = document.createElement('a')
